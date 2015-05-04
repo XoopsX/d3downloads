dc01c24e62493138f6e3e9b7492a5c8b64864dda
nao-pon <hypweb@gmail.com>
2015-05-04 14:23:18 +0900

    d3downloads version 1.49.4

--- xoops_trust_path/modules/d3downloads/xoops_version.php
+++ xoops_trust_path/modules/d3downloads/xoops_version.php
@@ -11,7 +11,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 $modversion['name'] = $mydirname ;
 $modversion['version'] = 1.49 ;
-$modversion['detailed_version'] = "1.49.3" ;
+$modversion['detailed_version'] = "1.49.4" ;
 $modversion['description'] = constant($constpref.'_DESC') ;
 $modversion['credits'] = "Xoops X(ten) Team and photosite";
 $modversion['author'] = "Xoops X(ten) Team based by photosite(http://www.photositelinks.com/)" ;
